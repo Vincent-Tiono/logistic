8952bd71077a72a3a835e1131829aa48351b252e
Vincent Octavian Tiono <vincentoctaviant@gmail.com>
2026-07-17 14:19:07 +0700

    database

---
+++ clean/databasemlp_phpmyadmin_cleaned.sql
@@ -0,0 +1,68 @@
+CREATE DATABASE IF NOT EXISTS `databasemlp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `databasemlp`;
+
+-- MySQL dump 10.13  Distrib 9.6.0, for macos26.4 (arm64)
+--
+-- Host: 127.0.0.1    Database: databasemlp
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `databasemlp`
+--
+
+--
+-- Table structure for table `usermlp`
+--
+
+DROP TABLE IF EXISTS `usermlp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usermlp` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `jabatan` varchar(100) DEFAULT NULL,
+  `divisi` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usermlp`
+--
+
+LOCK TABLES `usermlp` WRITE;
+/*!40000 ALTER TABLE `usermlp` DISABLE KEYS */;
+INSERT INTO `usermlp` VALUES ('admin','admin123','Admin','IT','2026-06-29 01:38:51'),('chai','chai','Dept. Head','Operation','2026-06-29 01:39:23');
+/*!40000 ALTER TABLE `usermlp` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'databasemlp'
+--
+--
+-- WARNING: can't read the INFORMATION_SCHEMA.libraries table. It's most probably an old server 8.0.41.
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-07-17 11:47:43
